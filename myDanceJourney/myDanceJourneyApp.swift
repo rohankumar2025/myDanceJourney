8694ae7fc804840dacb81ac4cf4fd6a732d82132
Rohan Kumar <rohankumar@ucsb.edu>
2022-09-06 14:06:32 -0700

    Initial Commit

---
+++ myDanceJourney/myDanceJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myDanceJourneyApp.swift
+//  myDanceJourney
+//
+//  Created by Rohan Kumar on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct myDanceJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
